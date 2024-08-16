a876a567f47e68fba7d56a2f75ed324cb72e7592
Xitrin <eraclab@gmail.com>
2024-08-16 19:31:35 +0000

    Add VM_NAME and GI_NAME vars, Split long lines, Add removing storage and nvram through virsh

--- vm.sh
+++ vm.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-w=$(virt-sysprep --list-operations | egrep -v 'fs-uuids|ssh-hostkeys|ssh-userdir' | awk '{ printf "%s,", $1}' | sed 's/,$//')
-virt-clone --original $1 --name $2 --auto-clone
-sudo virt-sysprep -d $2 --hostname $2 --enable $w --keep-user-accounts jenkins --firstboot-command 'dpkg-reconfigure openssh-server'
-virsh start $2
+GI_NAME=$1
+VM_NAME=$2
+w=$(virt-sysprep --list-operations | \
+	egrep -v 'fs-uuids|ssh-hostkeys|ssh-userdir' | \
+	awk '{ printf "%s,", $1}' | sed 's/,$//')
+virt-clone --original $GI_NAME --name $VM_NAME --auto-clone
+sudo virt-sysprep -d $VM_NAME --hostname $VM_NAME --enable $w \
+       	--keep-user-accounts jenkins \
+	--firstboot-command 'dpkg-reconfigure openssh-server'
+virsh start $VM_NAME
 sleep 20
-IP=$(virsh net-dhcp-leases default | grep $2 | awk '{ print $5}')
+IP=$(virsh net-dhcp-leases default | grep $VM_NAME | awk '{ print $5}')
 CIP=${IP%???}
-ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} systemctl status nginx
+ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} \
+	systemctl status nginx
 curl ${CIP}:80
 sleep 5
-virsh destroy $2
-virsh undefine $2
-sudo rm /var/lib/libvirt/images/$2.qcow2
+virsh destroy $VM_NAME
+virsh undefine $VM_NAME --remove-all-storage --nvram
