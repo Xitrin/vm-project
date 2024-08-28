7ff2f7762ac456c40610c1bceda9424f1b32c081
Xitrin <eraclab@gmail.com>
2024-08-27 23:25:37 -0400

    Add deploy.sh changes

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ VM_NAME=$1
 IP=$(virsh net-dhcp-leases default | grep $VM_NAME | awk '{ print $5}')
 CIP=${IP%???}
 ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} \
-        sudo apt install -y nginx &&
+        sudo apt install -y nginx &
 	sleep 10
