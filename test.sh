a16f1b59079ab0b390cafc0575b4cc025dd10572
Xitrin <eraclab@gmail.com>
2024-08-27 23:24:46 -0400

    Add test.sh changes

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@ VM_NAME=$1
 IP=$(virsh net-dhcp-leases default | grep $VM_NAME | awk '{ print $5}')
 CIP=${IP%???}
 ssh jenkins@${CIP} sudo ufw status &&
-        sleep 5 &&
         sudo systemctl status nginx
 curl ${CIP}:80
