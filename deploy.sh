54844adf26a632adaf6c10d39bfb83a29964fcb0
Xitrin <eraclab@gmail.com>
2024-08-29 23:19:33 -0400

    Add playbook

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 VM_NAME=$1
 IP=$(virsh net-dhcp-leases default | grep $VM_NAME | awk '{ print $5}')
 CIP=${IP%???}
-ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} \
-        sudo apt install -y nginx &
-	sleep 10
+ansible-playbook playbook-nginx.yml
+# ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} \
+#         sudo apt install -y nginx &
+# 	sleep 10
