0e03134278d3f39e428f3536bd0e0f29e2d28afb
Xitrin <eraclab@gmail.com>
2024-09-05 19:53:34 -0400

    Add sudo to docker calls

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ VM_NAME=$1
 IP=$(virsh net-dhcp-leases default | grep $VM_NAME | awk '{ print $5}')
 CIP=${IP%???}
 ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} \
-    docker pull nginx \
-    docker run --name docker-nginx -p 80:80 -d nginx
+    sudo docker pull nginx \
+    sudo docker run --name docker-nginx -p 80:80 -d nginx
 # ansible-playbook -i ansible/inventory/hosts ansible/playbook-nginx.yml
 # ssh -o "StrictHostKeyChecking=accept-new" jenkins@${CIP} \
 #         sudo apt install -y nginx &
