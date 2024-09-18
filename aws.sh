00c7d60679148df4ce36314b0c15e343412b3ca6
Xitrin <eraclab@gmail.com>
2024-09-17 21:27:45 -0400

    Modify Instance creation

--- aws.sh
+++ aws.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-aws ec2 run-instances \
+
+USER_DATA_FILE="install-nginx.sh"
+
+ID=$(aws ec2 run-instances \
     --region us-east-1 \
     --image-id ami-0ebfd941bbafe70c6 \
     --instance-type t2.micro \
     --key-name MyKeyPair \
     --security-group-ids sg-053db75f5fff9aefe \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyNginxInstance}]' \
-    --user-data '#!/bin/bash
-                yum update -y
-                amazon-linux-extras install nginx1 -y
-                systemctl start nginx
-                systemctl enable nginx' \
-    --query 'Instances[0].InstanceId'
+    --user-data file://"$USER_DATA_FILE" \
+    --query 'Instances[0].InstanceId' \
+    --output text)
+echo $ID
+
+IP=$(aws ec2 describe-instances \
+    --instance-ids "$ID" \
+    --query 'Reservations[*].Instances[*].PublicIpAddress' \
+    --output text)
+echo $IP
+
+sleep 20
+
+curl http://"$IP"
