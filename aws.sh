1b96b84f648433b37d7d3efc49b2cd6f3baef81e
Xitrin <eraclab@gmail.com>
2024-09-17 21:47:59 -0400

    Add more wait time for nginx to start

--- aws.sh
+++ aws.sh
@@ -2,24 +2,31 @@
 
 USER_DATA_FILE="install-nginx.sh"
 
-ID=$(aws ec2 run-instances \
+ID=$(
+aws ec2 run-instances \
     --region us-east-1 \
     --image-id ami-0ebfd941bbafe70c6 \
     --instance-type t2.micro \
     --key-name MyKeyPair \
     --security-group-ids sg-053db75f5fff9aefe \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyNginxInstance}]' \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=nginxer}]' \
     --user-data file://"$USER_DATA_FILE" \
     --query 'Instances[0].InstanceId' \
-    --output text)
+    --output text
+    )
 echo $ID
 
-IP=$(aws ec2 describe-instances \
+IP=$(
+aws ec2 describe-instances \
     --instance-ids "$ID" \
     --query 'Reservations[*].Instances[*].PublicIpAddress' \
-    --output text)
+    --output text
+    )
 echo $IP
 
-sleep 20
+sleep 60
+ssh -i MyKeyPair.pem -o "StrictHostKeyChecking=accept-new" \
+    ec2-user@$IP curl localhost
+curl $IP
 
-curl http://"$IP"
+aws ec2 terminate-instances --instance-ids "$ID"
